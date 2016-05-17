4ba1bdd7f29b48d90c5e90b652186630712fccb2
bio4554 <bioshock4554@gmail.com>
2016-05-16 21:13:43 -0500

    Add basic question check

--- app/src/main/java/com/school/bio4554/fastlanealgebra/MainGame.java
+++ app/src/main/java/com/school/bio4554/fastlanealgebra/MainGame.java
@@ -1,10 +1,14 @@
 package com.school.bio4554.fastlanealgebra;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -16,6 +20,9 @@ import java.util.Vector;
 public class MainGame extends AppCompatActivity{
 
     Random rand = new Random();
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel mChannel;
+    WifiP2pManager mManager;
 
     public int getRand(int min, int max) {
         int val = rand.nextInt(max) + min;
@@ -26,12 +33,15 @@ public class MainGame extends AppCompatActivity{
     Vector<String> answers = new Vector<String>(4);
     int currentquestion; //Index of current question in vector
     int currentanswer; //Radio button with answer
+    int prechoice = 0;
 
     TextView questiontext;
+    RadioGroup radiogroup;
     RadioButton answer1;
     RadioButton answer2;
     RadioButton answer3;
     RadioButton answer4;
+    ImageView redCar;
 
     private void addQuestion(String ques, String ans) {
         questions.addElement(ques);
@@ -52,12 +62,29 @@ public class MainGame extends AppCompatActivity{
         initQuestions();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
         questiontext = (TextView) findViewById(R.id.questionText);
         answer1 = (RadioButton)findViewById(R.id.answerChoice1);
         answer2 = (RadioButton) findViewById(R.id.answerChoice2);
         answer3 = (RadioButton) findViewById(R.id.answerChoice3);
         answer4 = (RadioButton) findViewById(R.id.answerChoice4);
+        radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
         updateQuestion();
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        redCar = (ImageView) findViewById(R.id.redCar);
     }
 
     public void advanceRed(View view) {
@@ -67,7 +94,10 @@ public class MainGame extends AppCompatActivity{
 
     public void updateQuestion() {
         currentanswer = getRand(1,4);
-        currentquestion = getRand(0, questions.capacity());
+        do {
+            currentquestion = getRand(0, questions.capacity());
+        } while(currentquestion == prechoice);
+        prechoice = currentquestion;
         for(int i = 1; i < 5; i++) {
             switch (i) {
                 case 1:
@@ -146,5 +176,25 @@ public class MainGame extends AppCompatActivity{
             }
         }
         questiontext.setText(questions.elementAt(currentquestion));
+    }
+
+    public void checkQuestion(View view) {
+        if(radiogroup.getCheckedRadioButtonId()!=-1){
+            int id= radiogroup.getCheckedRadioButtonId();
+            View radioButton = radiogroup.findViewById(id);
+            int radioId = radiogroup.indexOfChild(radioButton);
+            RadioButton btn = (RadioButton) radiogroup.getChildAt(radioId);
+            String selection = (String) btn.getText();
+            System.out.println("Found selected button!");
+            if(selection.equals(answers.elementAt(currentquestion))) {
+                System.out.println("Correct!");
+                redCar.setPadding(redCar.getPaddingLeft()+10, 0 ,0 ,0);
+                updateQuestion();
+            } else {
+                System.out.println("Wrong!");
+            }
+        } else {
+            System.out.println("No button was selected");
+        }
     }
 }
