a71dfb0e04daf3c084300cce32118dac3ad3e7d9
bio4554 <bioshock4554@gmail.com>
2016-05-17 21:13:27 -0500

    Clean up code

--- app/src/main/java/com/school/bio4554/fastlanealgebra/MainGame.java
+++ app/src/main/java/com/school/bio4554/fastlanealgebra/MainGame.java
@@ -15,12 +15,11 @@ import android.widget.Toast;
 import java.util.Random;
 import java.util.Vector;
 
-import io.github.kexanie.library.MathView;
 
 /**
- * Created by bio4554 on 5/16/2016.
+ * Created by Austin Childress on 5/16/2016. Made for Algebra I
  */
-public class MainGame extends AppCompatActivity{
+public class MainGame extends AppCompatActivity {
 
     Random rand = new Random();
     private final IntentFilter intentFilter = new IntentFilter();
@@ -28,16 +27,14 @@ public class MainGame extends AppCompatActivity{
     WifiP2pManager mManager;
 
     public int getRand(int min, int max) {
-        int val = rand.nextInt(max) + min;
-        return val;
+        return rand.nextInt(max) + min;
     }
 
-    Vector<String> questions = new Vector<String>(4);
-    Vector<String> answers = new Vector<String>(4);
+    Vector<String> questions = new Vector<>(4);
+    Vector<String> answers = new Vector<>(4);
     int currentquestion; //Index of current question in vector
     int currentanswer; //Radio button with answer
     int prechoice = 0;
-    String test = "$$\\\\sum_{i=0}^n i^2 = \\\\frac{(n^2+n)(2n+1)}{6}$$";
 
     TextView questiontext;
     RadioGroup radiogroup;
@@ -62,7 +59,6 @@ public class MainGame extends AppCompatActivity{
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         initQuestions();
@@ -82,7 +78,7 @@ public class MainGame extends AppCompatActivity{
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
         questiontext = (TextView) findViewById(R.id.questionText);
-        answer1 = (RadioButton)findViewById(R.id.answerChoice1);
+        answer1 = (RadioButton) findViewById(R.id.answerChoice1);
         answer2 = (RadioButton) findViewById(R.id.answerChoice2);
         answer3 = (RadioButton) findViewById(R.id.answerChoice3);
         answer4 = (RadioButton) findViewById(R.id.answerChoice4);
@@ -95,18 +91,14 @@ public class MainGame extends AppCompatActivity{
         greenCar = (ImageView) findViewById(R.id.greenCar);
     }
 
-    public void advanceRed(View view) {
-        ImageView redCar = (ImageView)findViewById(R.id.redCar);
-        redCar.setPadding(redCar.getPaddingLeft()+10, 0, 0, 0);
-    }
 
     public void updateQuestion() {
-        currentanswer = getRand(1,4);
+        currentanswer = getRand(1, 4);
         do {
             currentquestion = getRand(0, questions.capacity());
-        } while(currentquestion == prechoice);
+        } while (currentquestion == prechoice);
         prechoice = currentquestion;
-        for(int i = 1; i < 5; i++) {
+        for (int i = 1; i < 5; i++) {
             switch (i) {
                 case 1:
                     answer1.setText(answers.elementAt(getRand(0, questions.capacity())));
@@ -114,17 +106,19 @@ public class MainGame extends AppCompatActivity{
                 case 2:
                     do {
                         answer2.setText(answers.elementAt(getRand(0, questions.capacity())));
-                    } while(answer2.getText().equals(answer1.getText()));
+                    } while (answer2.getText().equals(answer1.getText()));
                     break;
                 case 3:
                     do {
                         answer3.setText(answers.elementAt(getRand(0, questions.capacity())));
-                    } while((answer3.getText().equals(answer1.getText())) || (answer3.getText().equals(answer2.getText())));
+                    }
+                    while ((answer3.getText().equals(answer1.getText())) || (answer3.getText().equals(answer2.getText())));
                     break;
                 case 4:
                     do {
                         answer4.setText(answers.elementAt(getRand(0, questions.capacity())));
-                    } while((answer4.getText().equals(answer1.getText())) || (answer4.getText().equals(answer2.getText())) || (answer4.getText().equals(answer3.getText())));
+                    }
+                    while ((answer4.getText().equals(answer1.getText())) || (answer4.getText().equals(answer2.getText())) || (answer4.getText().equals(answer3.getText())));
                     break;
             }
         }
@@ -143,41 +137,45 @@ public class MainGame extends AppCompatActivity{
                 break;
         }
 
-        for(int i = 1; i < 5; i++) {
+        for (int i = 1; i < 5; i++) {
             switch (i) {
                 case 1:
-                    if(i != currentanswer) {
-                        if(answer1.getText().equals(answers.elementAt(currentquestion))) {
+                    if (i != currentanswer) {
+                        if (answer1.getText().equals(answers.elementAt(currentquestion))) {
                             do {
                                 answer1.setText(answers.elementAt(getRand(0, questions.capacity())));
-                            } while((answer1.getText().equals(answer2.getText())) || (answer1.getText().equals(answer3.getText())) || (answer1.getText().equals(answer4.getText())));
+                            }
+                            while ((answer1.getText().equals(answer2.getText())) || (answer1.getText().equals(answer3.getText())) || (answer1.getText().equals(answer4.getText())));
                         }
                     }
                     break;
                 case 2:
-                    if(i != currentanswer) {
-                        if(answer2.getText().equals(answers.elementAt(currentquestion))) {
+                    if (i != currentanswer) {
+                        if (answer2.getText().equals(answers.elementAt(currentquestion))) {
                             do {
                                 answer2.setText(answers.elementAt(getRand(0, questions.capacity())));
-                            } while((answer2.getText().equals(answer1.getText())) || (answer2.getText().equals(answer3.getText())) || (answer2.getText().equals(answer4.getText())));
+                            }
+                            while ((answer2.getText().equals(answer1.getText())) || (answer2.getText().equals(answer3.getText())) || (answer2.getText().equals(answer4.getText())));
                         }
                     }
                     break;
                 case 3:
-                    if(i != currentanswer) {
-                        if(answer3.getText().equals(answers.elementAt(currentquestion))) {
+                    if (i != currentanswer) {
+                        if (answer3.getText().equals(answers.elementAt(currentquestion))) {
                             do {
                                 answer3.setText(answers.elementAt(getRand(0, questions.capacity())));
-                            } while((answer3.getText().equals(answer2.getText())) || (answer3.getText().equals(answer1.getText())) || (answer3.getText().equals(answer4.getText())));
+                            }
+                            while ((answer3.getText().equals(answer2.getText())) || (answer3.getText().equals(answer1.getText())) || (answer3.getText().equals(answer4.getText())));
                         }
                     }
                     break;
                 case 4:
-                    if(i != currentanswer) {
-                        if(answer4.getText().equals(answers.elementAt(currentquestion))) {
+                    if (i != currentanswer) {
+                        if (answer4.getText().equals(answers.elementAt(currentquestion))) {
                             do {
                                 answer4.setText(answers.elementAt(getRand(0, questions.capacity())));
-                            } while((answer4.getText().equals(answer2.getText())) || (answer4.getText().equals(answer3.getText())) || (answer4.getText().equals(answer1.getText())));
+                            }
+                            while ((answer4.getText().equals(answer2.getText())) || (answer4.getText().equals(answer3.getText())) || (answer4.getText().equals(answer1.getText())));
                         }
                     }
                     break;
@@ -188,17 +186,17 @@ public class MainGame extends AppCompatActivity{
     }
 
     public void checkQuestion(View view) {
-        if(radiogroup.getCheckedRadioButtonId()!=-1){
-            int id= radiogroup.getCheckedRadioButtonId();
+        if (radiogroup.getCheckedRadioButtonId() != -1) {
+            int id = radiogroup.getCheckedRadioButtonId();
             View radioButton = radiogroup.findViewById(id);
             int radioId = radiogroup.indexOfChild(radioButton);
             RadioButton btn = (RadioButton) radiogroup.getChildAt(radioId);
             String selection = (String) btn.getText();
             System.out.println("Found selected button!");
-            if(selection.equals(answers.elementAt(currentquestion))) {
+            if (selection.equals(answers.elementAt(currentquestion))) {
                 System.out.println("Correct!");
                 disToast("Correct!");
-                redCar.setPadding(redCar.getPaddingLeft()+50, 0 ,0 ,0);
+                redCar.setPadding(redCar.getPaddingLeft() + 50, 0, 0, 0);
                 updateQuestion();
                 updatePlayerc();
             } else {
@@ -215,25 +213,24 @@ public class MainGame extends AppCompatActivity{
 
     public void updatePlayerc() {
         int correct;
-        correct = getRand(0,10);
+        correct = getRand(0, 10);
 
-        if(correct > 5) {
-           blueCar.setPadding(blueCar.getPaddingLeft()+50, 0, 0, 0);
+        if (correct > 5) {
+            blueCar.setPadding(blueCar.getPaddingLeft() + 50, 0, 0, 0);
         }
 
-        correct = getRand(0,10);
+        correct = getRand(0, 10);
 
-        if(correct > 5) {
-            greenCar.setPadding(greenCar.getPaddingLeft()+50, 0, 0, 0);
+        if (correct > 5) {
+            greenCar.setPadding(greenCar.getPaddingLeft() + 50, 0, 0, 0);
         }
     }
 
     public void disToast(String name) {
         Context context = getApplicationContext();
-        CharSequence text = name;
         int duration = Toast.LENGTH_SHORT;
 
-        Toast toast = Toast.makeText(context, text, duration);
+        Toast toast = Toast.makeText(context, name, duration);
         toast.show();
     }
 }
